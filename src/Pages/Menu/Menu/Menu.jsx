0e7ffcfbe4c3ffa607a435b7bbe839ddad30157f
Shakil Ahmed <shakilahmedshuvo612@gmail.com>
2023-05-24 19:23:01 +0600

    add menu page info and loop the data and show the data

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,10 +1,24 @@
 // react helmet async
 import { Helmet } from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
-// import menuImg from '../../../assets/menu/menu-bg.png';
+import useMenu from '../../../hooks/useMenu';
+import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
+import MenuCategory from '../MenuCategory/MenuCategory';
+// import img
 import menuImg from "../../../assets/menu/banner3.jpg";
+import dessertImg from "../../../assets/menu/dessert-bg.jpeg";
+import pizzaImg from "../../../assets/menu/pizza-bg.jpg";
+import saladImg from "../../../assets/menu/salad-bg.jpg";
+import soupImg from "../../../assets/menu/soup-bg.jpg";
 
 const Menu = () => {
+    // load the data
+    const [menu] = useMenu();
+    const desserts = menu.filter(item => item.category === "dessert");
+    const soup = menu.filter(item => item.category === "soup");
+    const salad = menu.filter(item => item.category === "salad");
+    const pizza = menu.filter(item => item.category === "pizza");
+    const offered = menu.filter(item => item.category === "offered");
     return (
         <div>
             {/* using helmet async start */}
@@ -19,7 +33,49 @@ const Menu = () => {
                 img={menuImg}
                 title={"Our Menu"}
             ></Cover>
+
+            {/* main cover */}
+            <SectionTitle
+                subHeading={"Don't Miss"}
+                heading={"Today's Offer"}
+            ></SectionTitle>
+
+            {/* loop and map section start */}
+
+            {/* offered menu items */}
+            <MenuCategory
+                items={offered}
+            ></MenuCategory>
+
+            {/* desserts menu items */}
+            <MenuCategory
+                items={desserts}
+                title={"Dessert"}
+                img={dessertImg}
+            ></MenuCategory>
+
+            {/* pizza menu items */}
+            <MenuCategory
+                items={pizza}
+                title={"Pizza"}
+                img={pizzaImg}
+            ></MenuCategory>
+
+            {/* salad menu items */}
+            <MenuCategory
+                items={salad}
+                title={"Salad"}
+                img={saladImg}
+            ></MenuCategory>
+
+            {/* soup menu items */}
+            <MenuCategory
+                items={soup}
+                title={"Soup"}
+                img={soupImg}
+            ></MenuCategory>
             
+            {/* loop and map section end */}
         </div>
     );
 };
--- src/Pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/Pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,8 +1,26 @@
+import Cover from "../../Shared/Cover/Cover";
+import MenuItem from "../../Shared/MenuItem/MenuItem";
 
-const MenuCategory = () => {
+const MenuCategory = ({ items, title, img }) => {
     return (
-        <div>
-
+        <div className="pt-8">
+            {/* cover img */}
+            {
+                title &&
+                <Cover
+                    img={img}
+                    title={title}
+                ></Cover>
+            }
+            <div className="grid md:grid-cols-2 font-bold gap-10 mt-16">
+                {/* show the card and the map section and output in the menu category */}
+                {
+                    items.map(item => <MenuItem
+                        key={item._id}
+                        item={item}
+                    ></MenuItem>)
+                }
+            </div>
         </div>
     );
 };
