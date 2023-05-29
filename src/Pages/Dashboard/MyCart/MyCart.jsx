9b65ddc48969488ea4117e449b7c349086ab4407
Shakil Ahmed <shakilahmedshuvo612@gmail.com>
2023-05-30 00:28:45 +0600

    add cart table

--- src/Pages/Dashboard/MyCart/MyCart.jsx
+++ src/Pages/Dashboard/MyCart/MyCart.jsx
@@ -6,13 +6,13 @@ const MyCart = () => {
     console.log(cart);
     const total = cart.reduce((sum, item) => item.price + sum, 0)
     return (
-        <div className="uppercase">
+        <div>
             <Helmet>
                 <title>
-                    Bistro | My Cart
+                    BISTRO | MY CART
                 </title>
             </Helmet>
-            <div>
+            <div className="uppercase font-semibold flex justify-evenly items-center">
                 <h3 className="text-3xl">
                     Total Items: {cart.length}
                 </h3>
@@ -20,8 +20,69 @@ const MyCart = () => {
                     Total Price: ${total}
                 </h3>
                 <button className="btn  btn-warning btn-sm">
-                    Pay Now
+                    Pay
                 </button>
+            </div>
+            {/* table start */}
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>
+                                #
+                            </th>
+                            <th>
+                                Food
+                            </th>
+                            <th>
+                                Item Name
+                            </th>
+                            <th>
+                                Price
+                            </th>
+                            <th>
+                                Action
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            cart.map((item, index) => <tr
+                                key={item._id}
+                            >
+                                <td>
+                                    {index + 1}
+                                </td>
+                                <td>
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle w-12 h-12">
+                                            <img
+                                                src={item.image}
+                                                alt="Food Image" />
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    Zemlak, Daniel and Leannon
+                                    <br />
+                                    <span className="badge badge-ghost badge-sm">
+                                        Desktop Support Technician
+                                    </span>
+                                </td>
+                                <td>
+                                    Purple
+                                </td>
+                                <td>
+                                    <button className="btn btn-ghost btn-xs">
+                                        details
+                                    </button>
+                                </td>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
             </div>
         </div>
     );
